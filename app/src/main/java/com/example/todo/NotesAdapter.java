c09752bcbeaadda9b032e39963d96451c3ae4bb1
Nikhil Alex <nikhil.wtp@gmail.com>
2020-07-13 14:52:21 +0530

    notes almost working. fix fab

---
+++ app/src/main/java/com/example/todo/NotesAdapter.java
@@ -0,0 +1,78 @@
+package com.example.todo;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+public class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.MyViewHolder> {
+
+    private Context context;
+    private List<Note> notesList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView note;
+        public TextView timestamp;
+
+        public MyViewHolder(View view) {
+            super(view);
+            note = view.findViewById(R.id.note);
+            timestamp = view.findViewById(R.id.timestamp);
+        }
+    }
+
+
+    public NotesAdapter(Context context, List<Note> notesList) {
+        this.context = context;
+        this.notesList = notesList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.note_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Note note = notesList.get(position);
+
+        holder.note.setText(note.getNote());
+
+        // Formatting and displaying timestamp
+        holder.timestamp.setText(formatDate(note.getTimestamp()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return notesList.size();
+    }
+
+    /**
+     * Formatting timestamp to `MMM d` format
+     * Input: 2018-02-21 00:15:42
+     * Output: Feb 21
+     */
+    private String formatDate(String dateStr) {
+        try {
+            SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            Date date = fmt.parse(dateStr);
+            SimpleDateFormat fmtOut = new SimpleDateFormat("MMM d");
+            return fmtOut.format(date);
+        } catch (ParseException e) {
+
+        }
+
+        return "";
+    }
+}
